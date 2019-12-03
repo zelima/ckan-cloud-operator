1226686477394015daafd224a56e8f7be35690f1
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2019-12-03 18:23:16 +0400

    install socat

--- .travis.sh
+++ .travis.sh
@@ -27,6 +27,8 @@ elif [ "${1}" == "install-tools" ]; then
      helm version --client && rm ./get_helm.sh
     echo Helm Installed Successfully!
 
+    sudo apt-get update && sudo apt-get install socat
+
     echo Instalation Complete && exit 0
 
 elif [ "${1}" == "script" ]; then
